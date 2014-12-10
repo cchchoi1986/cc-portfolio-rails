4668c5877e43eebe5b2c0a5f55abcf254363009f
cchchoi1986 <cchchoi1986@gmail.com>
2014-12-10 09:46:19 +0800

    redirect links

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
   def index
-    
+    #don't have logics here
+    # so we are just going to render page 'index.html'
+  end
+
+  def dog
+    # @num = 1
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
+  # path 'controller#method'
+
+  get '/dog' => 'static_pages#cat'
+  # dog_path -> '/dog'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
